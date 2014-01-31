fc69b29003d4c632203f4e1642d1bcd423b43892
Ilya Fadeev <ilya.fadeev@clickfree.com>
2014-01-31 11:48:07 -0500

    #fb: added filter input for Step3; styling: max-height for friend list

--- app/scripts/filters/emptyifblankorselected.js
+++ app/scripts/filters/emptyifblankorselected.js
@@ -9,4 +9,13 @@ angular.module('myappApp')
         return _.filter(fObject, function (o) {
             return fQuery && fReg.exec(o.name) || o.selected === true;
         });
+    }})
+    .filter('matchedOrSelected', function(){ return function(fObject, fQuery){
+        var fReg = new RegExp(fQuery, 'i');
+        window.fObject = fObject;
+        window.fQuery = fQuery;
+        window.fReg = fReg;
+        return _.filter(fObject, function (o) {
+            return fReg.exec(o.name) || o.selected === true;
+        });
     }});
