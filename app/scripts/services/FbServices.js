6f59274599e2cc97cd6adb4455af54067d416f3c
ilyavf <fadeev.ilya@gmail.com>
2014-04-17 17:35:40 -0400

    fixed url for FB direct message

--- app/scripts/services/FbServices.js
+++ app/scripts/services/FbServices.js
@@ -2,22 +2,25 @@
 
 angular.module('myappApp')
     .factory('FbServices', function ($q) {
+        const APP_URL_REQUEST = 'https://apps.facebook.com/kooboodle/?user={user_id}&requestsubject={subject_id}';
 
         // Public API here
         return {
             requestMsg: function (userId, subjectId) {
                 var deferred = $q.defer(),
                     promise = deferred.promise,
-                    urlFbApp = 'https://apps.facebook.com/kooboodle/',
+                    urlFbApp = APP_URL_REQUEST
+                        .replace('{user_id}', userId)
+                        .replace('{subject_id}', subjectId),
                     urlDirect = 'https://testb.kooboodle.com/_fb/canvas_app/';
 
                 FB.ui({
                     method: 'send',
                     to: userId,
-                    link: urlFbApp + '?requestsubject=' + subjectId
+                    link: urlFbApp
                 }, function (response) {
                     // no response according to https://developers.facebook.com/docs/reference/dialogs/send/
-                    console.log('[FbServices.requestMsg] resolved', response);
+                    console.log('[FbServices.requestMsg] resolved. Sent to ' + userId + ', link: ' + urlFbApp, response);
                     var result = response && response.success ? 'success' : 'error';
                     deferred.resolve({
                         status:  result,
